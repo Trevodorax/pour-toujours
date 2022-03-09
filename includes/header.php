70a5d9195569ab4641092bd9eb6d0016a0103e86
Olua Parrot <phvcgdx@gmail.com>
2022-03-09 12:39:21 +0100

    added some details and animation

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,16 @@
     <div></div>
     <div></div>
 </header>
-<header id="desktop-header">
-
+<header id="desktop-header" class="mobile-hidden">
+    <div>
+        <img src="images/pt_logo.svg">
+        <img src="images/text_logo.svg">
+    </div>
+    <nav>
+        <a>FAQ</a>
+        <a>Prestataires</a>
+        <a>Accès pro</a>
+        <a>Se connecter</a>
+    </nav>
 </header>
 <div id="header-wave"></div>
