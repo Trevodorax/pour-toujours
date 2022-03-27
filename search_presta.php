ce3aafc4928eeba0c1886cc467dad04464f92e40
Olua Parrot <phvcgdx@gmail.com>
2022-03-27 10:33:00 +0200

    started the presta search page

---
+++ search_presta.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('includes/common_head.php'); ?>
+        <link rel="stylesheet" href="style/search_presta.css">
+    </head>
+    <body>
+
+        <?php include('includes/header.php'); ?>
+
+        <main>
+            <h2>Nos prestataires</h2>
+            <p>Découvrez tous nos prestataires prêts à travailler à vos côtés pour un mariage réussi</p>
+            <div id="page-top">
+                <p>Filtrez <img src="images/filter.svg"></p>
+                <p>Triez par <img src="images/three_dots.svg"></p>
+            </div>
+            <main>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
+            </main>
+        </main>
+
+        <?php include('includes/footer.php'); ?>
+
+        <script src="scripts/index.js"></script>
+        <script src="scripts/search_presta.js"></script>
+    </body>
+</html>
