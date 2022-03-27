3df468f5ab29ac4d0ac6fd77292c5f2ec44c3306
Olua Parrot <phvcgdx@gmail.com>
2022-03-27 18:00:33 +0200

    made the structure of settings page, but it requires ajax and php to continue

---
+++ settings.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('includes/common_head.php'); ?>
+        <link rel="stylesheet" href="style/settings.css">
+    </head>
+    <body>
+
+        <?php include('includes/header.php'); ?>
+
+        <main>
+
+            <h2>Mes paramètres</h2>
+            <section id="settings-generaux">
+                <h3>Généraux</h3>
+                <p>Affichage sombre</p>
+                <p>Affichage sombre</p>
+                <p>Affichage sombre</p>
+            </section>
+
+            <section id="settings-confidentialite">
+                <h3>Confidentialité</h3>
+                <p>Mentions légales</p>
+                <p>Contactez l'équipe technique</p>
+                <p>Lier une nouvelle adresse e-mail à mon compte</p>
+            </section>
+
+            <section id="settings-profil">
+                <h3>Mon profil</h3>
+                <p>Personnaliser mes avatars</p>
+                <p>Modifier mon mot de passe</p>
+                <p>Supprimer mon compte</p>
+                <p>Lier une nouvelle adresse e-mail à mon compte</p>
+            </section>
+
+        </main>
+
+        <?php include('includes/footer.php'); ?>
+
+        <script src="scripts/index.js"></script>
+        <script src="scripts/settings.js"></script>
+    </body>
+</html>
