2c37e368330287c3cd8047439a595ab0bcb97204
Olua Parrot <phvcgdx@gmail.com>
2022-03-27 11:29:29 +0200

    finished layout on presta search page (responsive mobile and desktop)

--- search_presta.php
+++ search_presta.php
@@ -9,13 +9,27 @@
         <?php include('includes/header.php'); ?>
 
         <main>
-            <h2>Nos prestataires</h2>
-            <p>Découvrez tous nos prestataires prêts à travailler à vos côtés pour un mariage réussi</p>
-            <div id="page-top">
-                <p>Filtrez <img src="images/filter.svg"></p>
-                <p>Triez par <img src="images/three_dots.svg"></p>
+            <div id="desktop-page-top">
+                <div id="page-title">
+                    <h2>Nos prestataires</h2>
+                    <p>Découvrez tous nos prestataires prêts à travailler à vos côtés pour un mariage réussi</p>
+                </div>
+                <div id="page-top">
+                    <p>Filtrez <img src="images/filter.svg"></p>
+                    <p>Triez par <img src="images/three_dots.svg"></p>
+                </div>
             </div>
+            
             <section>
+                <div class="presta-card">
+                    <img src="images/home_circle.jpg">
+                    <div>
+                        <h3>Evelynn Delon</h3>
+                        <h4>Photographe</h4>
+                        <p>Orléans</p>
+                        <a>Contacter <img src="images/presta_contact_icon.svg"></a>
+                    </div>
+                </div>
                 <div class="presta-card">
                     <img src="images/home_circle.jpg">
                     <div>
