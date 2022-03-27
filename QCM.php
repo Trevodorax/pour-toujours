e23070a1fd2c0367b000c029f74a6685cfe3f06c
Olua Parrot <phvcgdx@gmail.com>
2022-03-27 19:43:11 +0200

    created begining of QCM page

---
+++ QCM.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <title>QCM - Pour Toujours</title>
+        <link rel="stylesheet" href="style/QCM.css">
+    </head>
+    <body>
+        <main>
+            <h2>Question 1/10</h2>
+            <form>
+                <p>Vous planifiez un mariage :</p>
+                <div>
+                    <input type="radio" name="type">
+                    <span>Civil</span>
+                </div>
+                <div>
+                    <input type="radio" name="type">
+                    <span>Religieux</span>
+                </div>
+                <div>
+                    <input type="radio" name="type">
+                    <span>Mixte</span>
+                </div>
+            </form>
+        </main>
+        <script src="scripts/QCM.js"></script>
+    </body>
+</html>
