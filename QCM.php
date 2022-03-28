c67f6e3a9661e0f7f4bf511fcbd4bcd935f80106
Olua Parrot <phvcgdx@gmail.com>
2022-03-28 19:47:49 +0200

    finished html and css structure of QCM form

--- QCM.php
+++ QCM.php
@@ -23,7 +23,17 @@
                     <input type="radio" name="type">
                     <span>Mixte</span>
                 </div>
+                <div>
+                    <input type="radio" name="type">
+                    <span>Peu importe</span>
+                </div>
+                <div class="form-navigation">
+                    <a onclick="change_form(1, false)">&lt-- précédent</a>
+                    <a onclick="change_form(1, true)">Suivant --&gt</a>
+                </div>
+                
             </form>
+            
         </main>
         <script src="scripts/QCM.js"></script>
     </body>
