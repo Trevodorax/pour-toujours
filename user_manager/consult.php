6f73ee549c8bb091232f063cbb32382527e341ed
Olua Parrot <phvcgdx@gmail.com>
2022-04-05 20:07:11 +0200

    we can now consult users in user manager

---
+++ user_manager/consult.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include('../includes/common_head.php'); ?>
+    </head>
+    <body>
+        <?php
+            include('../includes/db.php');
+
+            $q = 'SELECT * FROM PERSONNE WHERE id = :id';
+            $req = $bdd->prepare($q);
+            $req->execute([
+                'id' => $_GET['id']
+            ]);
+            $results = $req->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+
+        <main class="container">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Champ</th>
+                        <th>Valeur</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        foreach($results[0] as $key => $value){
+                            if($key == 'mot_de_passe'){
+                                echo '<tr>';
+                                echo '<td>' . $key . '</td>';
+                                echo '<td>' . 'hidden' . '</td>';
+                                echo '</tr>';
+                            }else{
+                                echo '<tr>';
+                                echo '<td>' . $key . '</td>';
+                                echo '<td>' . $value . '</td>';
+                                echo '</tr>';
+                            }
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </main>
+
+        <script src="scripts/index.js"></script>
+    </body>
+</html>
