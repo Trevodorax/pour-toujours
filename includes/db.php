d67add37b3039b4e3e34a2b9ce8354db861a6e31
Simon Damette <simon.damette@outlook.fr>
2022-04-05 13:26:47 +0200

    Inscription v2

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $bdd = new PDO('mysql:host=localhost:3306;dbname=pa2k22', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $bdd = new PDO('mysql:host=localhost:3306;dbname=POURTOUJOURS', 'root', 'root', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     }
     catch(Exception $e){
         die('Erreur : ' . $e->getMessage());
