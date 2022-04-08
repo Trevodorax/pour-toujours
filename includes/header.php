1d354cda195185a9a40173af23bd024815730b4b
Olua Parrot <phvcgdx@gmail.com>
2022-04-08 19:56:55 +0200

    fixed broken link in header

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@
     </a>
     <nav>
         <a href="FAQ.php">FAQ</a>
-        <a href="search_pro">Prestataires</a>
+        <a href="search_pro.php">Prestataires</a>
         <?php
 
         //display a different page based on what kind of user is logged in
@@ -37,7 +37,7 @@
 <div id="header-wave"></div>
 <nav id="burger-menu" class="desktop-hidden pouf">
     <!-- The burger menu isn't working yet -->
-    <a href="search_pro"><h3>Nos partenaires<img src="images/go_icon.svg"></h3></a>
+    <a href="search_pro.php"><h3>Nos partenaires<img src="images/go_icon.svg"></h3></a>
     <a href="create_account.php"><h3>Accès professionnels<img src="images/go_icon.svg"></h3></a>
     <a href="#"><h3>Trouvez l'inspiration<img src="images/go_icon.svg"></h3></a>
     <a href="FAQ.php"><h3>F.A.Q<img src="images/go_icon.svg"></h3></a>
