8b1dfcb95b2b3781467ca4a2adf46294d94a6952
Simon Damette <simon.damette@outlook.fr>
2022-04-10 20:29:25 +0200

    Save Messages

--- includes/control_pannel/messages.php
+++ includes/control_pannel/messages.php
@@ -18,40 +18,88 @@
     <div class="message-page-content">
         <section id="messages-list">
             <button id="new-conv-form-appear" class="btn btn-primary" formmethod="post" formaction="" name="newconversation">Créer une nouvelle conversation</button>
-            <form id="new-conv-form" class="" method="post" action="new_conversation.php">
+            <form id="new-conv-form" class="pouf" method="post" action="new_conversation.php">
                 <input type="email" name="email">
                 <input type="submit">
             </form>
-
             <?php
-                ?>
-                <a href="control_pannel.php?page=messages&destinataire=">
-                    <img src="images/message_pfp.jpg">
-                    <div><span>&nbspPour Toujours</span> - l'équipe<br>1 nouveau message</div>
-                </a>
-                <?php
-            ?>
+                $q = 'SELECT id, personne1, personne2 FROM conversation WHERE personne1 = :id OR personne2 = :id';
+                $req = $bdd->prepare($q);
+                $req->execute([
+                    'id' => $_SESSION['id']
+                ]);
+                $conversations = $req->fetchAll();
 
+                if(count($conversations) != 0){
+                    foreach ($conversations as $key => $value) {
+                        if ($conversations[$key][1] == $_SESSION['id']){
+                            $id_destinataire = $conversations[$key][2];
+                        }else{
+                            $id_destinataire = $conversations[$key][1];
+                        }
+
+                        $q = 'SELECT email, nomprefere FROM personne WHERE id = :id';
+                        $req = $bdd->prepare($q);
+                        $req->execute([
+                            'id' => $id_destinataire
+                        ]);
+                        $destinataires = $req->fetchAll();
+
+                            echo '  <a href="control_pannel.php?page=messages&destinataire=' . $destinataires[0][0] .'">
+                                                <img src="images/message_pfp.jpg">
+                                                <div><span>' . $destinataires[0][1] .'</span> - ' . $destinataires[0][0] .'<br>1 nouveau message</div>
+                                            </a>';
+                    }
+                }
+            ?>
         </section>
         <section id="conversation">
             <?php
-                $q = 'SELECT id FROM conversation WHERE ' . $_SESSION['id'] . ' = personne1 OR ' . $_SESSION['id'] . ' = personne2';
-                $req = $bdd->prepare($q);
-                $req->execute([
+                if (isset($_GET['destinataire']) && !empty($_GET['destinataire'])){
+                    $email_destinataire = htmlspecialchars($_GET['destinataire']);
+                }
 
-                ]);
-                $id_conversation = $req->fetchAll();
+                if(isset($email_destinataire) && !empty($email_destinataire)) {
+                    $q = 'SELECT id, email FROM personne WHERE email = :email';
+                    $req = $bdd->prepare($q);
+                    $req->execute([
+                        'email' => $email_destinataire
+                    ]);
+                    $destinataire = $req->fetchAll();
 
-                $q = 'SELECT contenu, id_auteur FROM message WHERE conversation = ' . $id_conversation[0][0];
-                $req = $bdd->prepare($q);
-                $req->execute([
-                ]);
-                $message = $req->fetchAll();
-                foreach ($message as $key => $value){
-                    if ($message[$key][1] == $_SESSION['id']){
-                        echo '<div class="to">' . $message[$key][0] .'</div>';
-                    }else{
-                        echo '<div class="from">' . $message[$key][0] .'</div>';
+                    if (count($destinataire) == 0) {
+                        header('location: control_pannel.php?page=messages&message=Aucun utilisateur trouvé');
+                        exit;
+                    }
+
+                    if ($email_destinataire == $destinataire[0][0]) {
+                        header('location: control_pannel.php?page=messages&message=L\'email ne correspond a aucun utilisateur');
+                        exit;
+                    }
+
+                    $q = 'SELECT id FROM conversation WHERE (personne1 = ? AND personne2 = ?) OR (personne1 = ? AND personne2 = ?)';
+                    $req = $bdd->prepare($q);
+                    $req->execute([
+                        $destinataire[0][0],
+                        $_SESSION['id'],
+                        $_SESSION['id'],
+                        $destinataire[0][0]
+                    ]);
+                    $conversation = $req->fetchAll();
+
+                    $q = 'SELECT contenu, id_auteur FROM message WHERE conversation = :conversation';
+                    $req = $bdd->prepare($q);
+                    $req->execute([
+                        'conversation' => $conversation[0][0]
+                    ]);
+                    $message = $req->fetchAll();
+
+                    foreach ($message as $key => $value) {
+                        if ($message[$key][1] == $_SESSION['id']) {
+                            echo '<div class="to">' . $message[$key][0] . '</div>';
+                        } else {
+                            echo '<div class="from">' . $message[$key][0] . '</div>';
+                        }
                     }
                 }
             ?>
--- new_conversation.php
+++ new_conversation.php
@@ -2,14 +2,14 @@
     session_start();
     include('includes/db.php');
     $email = htmlspecialchars($_POST['email']);
-    $q = 'SELECT id FROM personne WHERE email = :email';
+    $q = 'SELECT id, email FROM personne WHERE email = :email';
     $req = $bdd->prepare($q);
     $req->execute([
         'email' => $email
     ]);
-    $id2 = $req->fetchAll();
+    $id = $req->fetchAll();
 
-    if(count($id2) == 0){
+    if(count($id) == 0){
         header('location: control_pannel.php?page=messages&message=L\'email n\'existe pas');
         exit;
     }
@@ -18,6 +18,9 @@
     $req = $bdd->prepare($q);
     $results = $req->execute([
         'personne1' => $_SESSION['id'],
-        'personne2' => $id2[0][0]
+        'personne2' => $id[0][0]
     ]);
+
+    header('location: control_pannel.php?page=messages&message=Conversation créée&destinataire=' . $id[0][1]);
+    exit;
 ?>
