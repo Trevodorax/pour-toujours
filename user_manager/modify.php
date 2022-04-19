e7200048c021b2d965b587fd3480cd81a9d0dd76
Olua Parrot <phvcgdx@gmail.com>
2022-04-19 21:47:08 +0200

    user manager works and sends modification information to the modification page

--- user_manager/modify.php
+++ user_manager/modify.php
@@ -4,12 +4,12 @@
     //! check if user is admin
 
     $personne_columns = ['nomComplet', 'nomPrefere', 'date_naissance', 'genre', 'email', 'numero_tel', 'departement', 'date_inscription', 'estAdmin'];
-    $prestataire_columns = ['nomEntreprise', 'telPro', 'emailPro', 'metier', 'description', 'photoProfil', 'lienSiteWeb', 'personne'];
-    $utilisateur_columns = ['preferencesQCM', 'avatar', 'personne'];
+    $prestataire_columns = ['nomEntreprise', 'telPro', 'emailPro', 'metier', 'description', 'photoProfil', 'lienSiteWeb'];
+    $utilisateur_columns = ['preferences_qcm', 'avatar'];
 
-    $modified_table = (in_array($_GET['column'], $personne_columns) ? 'PERSONNE' : (in_array($_GET['column'], $prestataire_columns) ? 'PRESTATAIRE' : ((in_array($_GET['column'], $utilisateur_columns) ? 'UTILISATEUR' : 'ERREUR'))));
+    $modified_table = (in_array($_POST['column'], $personne_columns) ? 'PERSONNE' : (in_array($_POST['column'], $prestataire_columns) ? 'PRESTATAIRE' : ((in_array($_POST['column'], $utilisateur_columns) ? 'UTILISATEUR' : 'ERREUR'))));
     if($modified_table == 'ERREUR'){
-        header('location: consult.php?id=' . $_GET['id']);
+        header('location: consult.php?id=' . $_POST['id']);
         exit;
     }
 ?>
@@ -27,15 +27,22 @@
             $q = 'SELECT * FROM PERSONNE WHERE id = :id';
             $req = $bdd->prepare($q);
             $req->execute([
-                'id' => $_GET['id']
+                'id' => $_POST['id']
             ]);
             $results = $req->fetchAll(PDO::FETCH_ASSOC);
         ?>
 
         <main class="container">
             <form>
+                <?php
 
-            </form>
+                var_dump($modified_table);
+                var_dump($_POST['id']);
+                var_dump($_POST['column']);
+                var_dump($_POST['new_content']);
+
+                ?>
+                </form>
         </main>
 
         <script src="scripts/index.js"></script>
