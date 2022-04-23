92c19b7100b0448bbc208fb5b784b70c37cb32db
Milo <lhilderal@myges.fr>
2022-04-23 21:24:16 +0200

    tried new technique for the canva sending buuuut not that yet...

--- save_signature.php
+++ save_signature.php
@@ -4,7 +4,8 @@ session_start();
 
 include('includes/db.php');
 
-if (isset($_POST['sign']) && !empty($_POST['sign'])){
+    if(isset($_FILES['image'])){
+    echo 'OK';
 
     $signbase64 = ($_POST['sign']);
     echo $signbase64;
@@ -12,14 +13,12 @@ if (isset($_POST['sign']) && !empty($_POST['sign'])){
     if(!file_exists($path)){
         mkdir($path, 0777,true);
     }
-     
+    
     //Transforming the string to get the image content
     $beforeSign = explode(';',$signbase64);
     $transformSign = explode(',' , $beforeSign[1]);
     $signature = base64_decode($transformSign[1]) ;
-
     $filename = $path . 'signature-'. time() .'.png';
-
      // saving the signature in the db : update because you can change ur signature.
      $q = "UPDATE prestataire SET signature = :signature WHERE personne = 1";
      $req = $bdd->prepare($q);
@@ -27,10 +26,13 @@ if (isset($_POST['sign']) && !empty($_POST['sign'])){
          'signature' => $filename,
         //  'personne' => $_SESSION['id']
      ]);
-
     $success = file_put_contents(__DIR__.$filename , $signature);
- 
-} else {
-    echo 'il manque des parametres';
-}
+
+
+
+    }
+    else if (isset($FILES['signature.png'])){ 
+        echo 'nope';
+    }    
+
 ?>
